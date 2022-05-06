3ce4fa8d989b1191ef144a65479a7cc046736261
harshanabandara <e16049@eng.pdn.ac.lk>
2022-05-06 14:24:55 +0530

    customized the code

---
+++ src/components/Avatar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Avatar() {
+  return <img src="../avatar.JPG"></img>;
+}
+
+export default Avatar;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="center">
+      <h1>Harshana Bandara</h1>
+      <p>University of Peradeniya</p>
+      <p>harshanabandaraofficial@gmail.com</p>
+    </div>
+  );
+}
+
+export default Header;
